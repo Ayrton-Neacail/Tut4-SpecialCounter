2ed2c4c74e0f88baa87f3aa5d1a1e885fc05d60b
Ayrton-Neacail <ayrtona90@gmail.com>
2016-03-16 10:25:04 +0200

    initial commit

---
+++ Tut4-Counter/count.h
@@ -0,0 +1,29 @@
+#pragma once;
+#define count.H;
+
+#include <iostream>
+#include <string.h> // includes libraries to be used in source file
+
+class SpecialCounter{
+
+public:   
+
+
+private:   
+	int start;
+	int stop;
+	int step;
+	int currentVal;
+
+
+
+
+
+
+
+
+};
+
+
+
+
